3d126a8db38b0e761a0aec33a21d3abfc4df2b8f
Eli Xian <elixian@gmail.com>
2015-01-30 23:57:56 -0600

    angularfire

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-var firebase = new Firebase("https://boiling-torch-2275.firebaseio.com/");
+var app = angular.module('FirebaseApp', ['firebase']);
 
-var app = angular.module('FirebaseApp', []);
+app.controller('FirebaseController', ['$scope', '$firebase', function ($scope, $firebase) {
 
-app.controller('FirebaseController', ['$scope', function ($scope) {
+	var ref = new Firebase("https://boiling-torch-2275.firebaseio.com/");
 
-	$scope.Messages = [];
+	$scope.Messages = $firebase(ref).$asArray();
 
 	$scope.sendMessage = function (message) {
-		var msg = {message: message}
-		firebase.push( msg );
-
+		$scope.Messages.$add( {message: message} );
 		$scope.fb.message = '';
 	}
-
-	firebase.on('child_added', function (snapshot) {
-		var message = snapshot.val();
-
-		$scope.Messages.push(message);
-	});
 
 }]);
