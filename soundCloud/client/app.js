e9e15182a05ce348f7ec0a5bef8012725cdc1847
Eli Xian <elixian@gmail.com>
2015-02-22 22:44:09 -0600

    embed user playlist with oEmbed

--- soundCloud/client/app.js
+++ soundCloud/client/app.js
@@ -5,20 +5,25 @@ $(function(){
 
     //get authorized user info
     SC.get('/me', function (me) { 
-      console.log('connected ' + me.username)
       getPlaylist(me.id)
     });
 
   });
 
-
+  //get playlist url
   var getPlaylist = function(userID){
     SC.get('/playlists', {user_id: userID}, function (playlist){
-      console.log(playlist);
-      _.each(playlist.tracks, function (track){
-        console.log(track);
-      });
+      playlistUrl = playlist[0].permalink_url;
+      getWidget(playlistUrl);
     })
+  }
+
+  //get widget for playlist and insert into DOM
+  var getWidget = function(url){
+    console.log('getting widget',url)
+    SC.oEmbed(url, {auto_play: true, iframe:true }, function (oEmbed){
+      $('.player').html(oEmbed.html);
+    });
   }
 
 })
